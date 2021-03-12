0ddddf12e98ceb91b640651dc9b29bde1c8c4904
ViktorGiggs <shostakv90@gmail.com>
2021-03-13 00:27:18 +0300

    12

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>{{title}}</h1>
+
+    <input v-model="searchItem" type="text" placeholder="Search">
+
+    <ul>
+      <li v-for="(item, index) in filteredItems" :key="index">{{item}}</li>
+    </ul>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return{
+      title: 'Масивный масив',
+      searchItem: '',
+      items:['BMW','Honda','audi','orange','lemon','pineapple','iron','zinc','copper']
+    }
+  },
+  computed:{
+    filteredItems() {
+      return this.items.filter(item => {
+        return item.toUpperCase().indexOf(this.searchItem.toUpperCase()) !==   -1
+      })
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div
+        v-for="(item, key) in subjects"
+        :key="key"
+    >
+      <div
+          v-if="item.type === type"
+      >
+        {{ item.name }}
+      </div>
+    </div>
+    <input
+        class="myText"
+        type="text"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['subjects'],
+  data() {
+    return {
+      type: '',
+    }
+  },
+  created() {
+    console.log(this.subjects);
+    console.log(this.$route);
+    this.type = this.$route.query.type
+  }
+}
+</script>
+
+<style scoped>
+.myText {
+  border: 1px solid black;
+  border-radius: 10px;
+}
+</style>
